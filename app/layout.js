20ef8a53617e7f205d723a5bfd3c160631c56eeb
joy11122 <joyhaldar112233@gmail.com>
2024-07-17 12:31:47 -0700

    doneghjjklf

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import "@/public/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./globals.css";
 import "@/public/booticon/font/bootstrap-icons.css";
 import Navbar from "@/app/component/Navbar";
@@ -10,8 +10,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
-    
+      <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
+          integrity="..."
+          crossorigin="anonymous"
+         defer 
+        ></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
       </Head>
       <body>
         <Navbar />
