2e2455231007d9bdeab3093d5bb445130a199205
joy11122 <joyhaldar112233@gmail.com>
2024-07-16 22:59:07 -0700

    fdf

--- app/layout.js
+++ app/layout.js
@@ -7,17 +7,20 @@ import Navbar from "@/app/component/Navbar";
 import Footer from "./component/Footer";
 import Line from "./component/Line";
 import { useEffect } from "react";
+import Head from "next/head";
 
 
 
 export default function RootLayout({ children }) {
-  useEffect(()=>{
-    require("bootstrap/dist/js/bootstrap.bundle")
-  })
 
 
   return (
     <html lang="en">
+<Head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</Head>
       <body>
         <Navbar />
         <Line />
