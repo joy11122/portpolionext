f72827bc8a83fb3aebc2c0fe36bde9decfdd7e44
joy11122 <119776174+joy11122@users.noreply.github.com>
2024-07-17 11:54:50 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./globals.css";
-import "@/public/booticon/font/bootstrap-icons.css";
-import Navbar from "@/app/component/Navbar";
-import Footer from "./component/Footer";
-import Line from "./component/Line";
+import './globals.css';
+import '@/public/booticon/font/bootstrap-icons.css';
+import Navbar from '@/app/component/Navbar';
+import Footer from './component/Footer';
+import Line from './component/Line';
 import Head from 'next/head';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
-    
+        <title>Joyanto Portpolio</title>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
+          integrity="sha384-ho+j7jyWK8fNQe+A12TFt4KTK3HltlWQ0gDZ1jbNkwHjzXbrHD6RtL8t6+c0Yo6N"
+          crossorigin="anonymous"
+          defer
+        ></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" defer></script>
       </Head>
       <body>
         <Navbar />
@@ -19,13 +25,6 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
       </body>
-      <script
-          src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
-          integrity="..."
-          crossorigin="anonymous"
-         defer 
-        ></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     </html>
   );
 }
