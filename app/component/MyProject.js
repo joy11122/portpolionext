5c0f96c87957b6619f548cce85743e5dae7f4b7e
joy11122 <119776174+joy11122@users.noreply.github.com>
2024-08-24 12:35:28 +0600

    Update MyProject.js
    Linked added

--- app/component/MyProject.js
+++ app/component/MyProject.js
@@ -1,43 +1,46 @@
 import React from "react";
 import Link from 'next/link';
+
 const MyProject = () => {
   return (
     <>
       <div className="container fullvh mb-5" data-aos="flip-down">
         <h2 className="text-center mb-3">My project</h2>
         <div className="row d-flex justify-content-center align-items-center gy-3 ">
-          <div className="col-md-4 col-lg-3 " data-aos="slide-left">
+          <div className="col-md-4 col-lg-3" data-aos="slide-left">
             <Link href="https://joy-nu.vercel.app/">
-    <img
-              className="w-100"
-              style={{ height: 350 }}
-              src="images/card1.jpg"
-              alt="hg"
-            />
-    <Link/>
+              <a>
+                <img
+                  className="w-100"
+                  style={{ height: 350 }}
+                  src="images/card1.jpg"
+                  alt="hg"
+                />
+              </a>
+            </Link>
           </div>
 
-          <div className="col-md-8 " data-aos="slide-right">
+          <div className="col-md-8" data-aos="slide-right">
             <img
-              className="w-100 "
+              className="w-100"
               style={{ height: 350 }}
               src="images/card1.jpg"
               alt="grt"
             />
           </div>
 
-          <div className="col-md-8 " data-aos="slide-left">
+          <div className="col-md-8" data-aos="slide-left">
             <img
-              className="w-100 "
+              className="w-100"
               style={{ height: 350 }}
               src="images/card1.jpg"
               alt="/rtr"
             />
           </div>
 
-          <div className="col-md-4  col-lg-3 " data-aos="slide-right">
+          <div className="col-md-4 col-lg-3" data-aos="slide-right">
             <img
-              className="w-100 "
+              className="w-100"
               style={{ height: 350 }}
               src="images/card2.jpg"
               alt="rtg"
